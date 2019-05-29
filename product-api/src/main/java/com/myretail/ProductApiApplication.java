cd49af2de78b1c0c1320b9b9067d21050a00a761
thulasiram <thulasiram.ponnam@in.verizon.com>
2019-05-29 12:39:18 +0530

    correcting spring cloud version product, productprice, product api service

--- product-api/src/main/java/com/myretail/ProductApiApplication.java
+++ product-api/src/main/java/com/myretail/ProductApiApplication.java
@@ -1,15 +1,10 @@
 package com.myretail;
 
-import org.apache.commons.lang.builder.ToStringBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.stereotype.Component;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
+@EnableEurekaClient
 @SpringBootApplication
 public class ProductApiApplication {
 
@@ -20,21 +15,21 @@ public class ProductApiApplication {
 }
 
 
-@Component
-class DiscoveryClientExample implements CommandLineRunner {
-
-    @Autowired
-    private DiscoveryClient discoveryClient;
-
-    @Override
-    public void run(String... strings) throws Exception {
-        discoveryClient.getInstances("product-service").forEach((ServiceInstance s) -> {
-            System.out.println(ToStringBuilder.reflectionToString(s));
-        });
-        
-        discoveryClient.getInstances("product-price-service").forEach((ServiceInstance s) -> {
-        	System.out.println(s.getUri());
-            System.out.println(ToStringBuilder.reflectionToString(s));
-        });
-    }
-}
+//@Component
+//class DiscoveryClientExample implements CommandLineRunner {
+//
+//    @Autowired
+//    private DiscoveryClient discoveryClient;
+//
+//    @Override
+//    public void run(String... strings) throws Exception {
+//        discoveryClient.getInstances("product-service").forEach((ServiceInstance s) -> {
+//            System.out.println(ToStringBuilder.reflectionToString(s));
+//        });
+//        
+//        discoveryClient.getInstances("product-price-service").forEach((ServiceInstance s) -> {
+//        	System.out.println(s.getUri());
+//            System.out.println(ToStringBuilder.reflectionToString(s));
+//        });
+//    }
+//}
